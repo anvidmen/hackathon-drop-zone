1d086e0be747936eb213571e6d235b137ed49793
Juan Omen <anvidmen@gmail.com>
2022-06-22 12:12:38 +0200

    created placeholder component

---
+++ src/components/Placeholder/Placeholder.jsx
@@ -0,0 +1,11 @@
+import { MdCloudUpload } from 'react-icons/md'
+import './styles.sass'
+
+const Placeholder = () => (
+  <div className='placeholder-preview'>
+    <MdCloudUpload className='icon' />
+    <p>ARASTRA TUS ARCHIVOS AQUÍ</p>
+  </div>
+)
+
+export default Placeholder
