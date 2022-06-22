073116e06fef1f282a25599e8486a9f69fa1a7b9
Juan Omen <anvidmen@gmail.com>
2022-06-22 21:57:02 +0200

    Refactored the drop zone field code

--- src/components/DropzoneField/DropzoneField.jsx
+++ src/components/DropzoneField/DropzoneField.jsx
@@ -1,36 +1,26 @@
-import { useDropzone } from "react-dropzone";
-import Placeholder from "components/Placeholder/Placeholder";
-import FilesPreview from "components/FilesPreview/FilesPreview";
+import { useDropzone } from 'react-dropzone'
+import Placeholder from 'components/Placeholder/Placeholder'
+import FilesPreview from 'components/FilesPreview/FilesPreview'
 import './styles.sass'
 
-const DropzoneField = (props) => {
-    const {
-        getRootProps,
-        getInputProps,
-        isDragAccept,
-        isFocused,
-        isDragReject,
-    } = useDropzone({
-      
-        onDrop: props.onDrop,
+const DropzoneField = ({files, fileErrors, onDrop}) => {
+    const { getRootProps, getInputProps, isDragAccept, isFocused, isDragReject } = useDropzone({
+        onDrop: onDrop,
         noClick: false,
         noKeyboard: true,
-    });
+    })
  
     return (
-
         <div
-            className=" container-field"
+            className='container-field'
             {...getRootProps({ isDragAccept, isFocused, isDragReject })}
         >
             <input {...getInputProps()} />
-            {props.files && props.files.length > 0 ?
-                <FilesPreview files={props.files} /> :
+            {files?.length ? files.map( file => <FilesPreview file={file} /> ):
                 <Placeholder />
             }
-
         </div>
-    );
+    )
 }
 
 export default DropzoneField
