1da155cc6dba0b5e44410c5e89e9ba37456f58bd
Juan Omen <anvidmen@gmail.com>
2022-06-22 17:38:17 +0200

    changed styles of file preview component size

--- src/components/FilesPreview/FilesPreview.jsx
+++ src/components/FilesPreview/FilesPreview.jsx
@@ -6,7 +6,7 @@ const FilesPreview = ({ files }) => (
         {console.log(files)}
             <img className='image-container' src={`${type.includes("application") ? 'https://upload.wikimedia.org/wikipedia/commons/0/0c/File_alt_font_awesome.svg': preview }`} alt='images prewiew' />
             <h2 className='details'>
-                {`${name.substring(0, 14)}`} - {(size / 1024000).toFixed(2)}MB
+                {`${name.substring(0, 12)}`} - {(size / 1024000).toFixed(2)}MB
             </h2>
         </div>
     ))
