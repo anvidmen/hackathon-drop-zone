6bd372623386affbdd9465dedf8556bb0958f774
Juan Omen <anvidmen@gmail.com>
2022-06-24 20:43:19 +0200

    refactored file preview code

--- src/components/FilePreview/FilePreview.jsx
+++ src/components/FilePreview/FilePreview.jsx
@@ -1,14 +1,16 @@
 import './styles.sass'
 
-const FilesPreview = ({ file }) => (
-    file?.map(( {preview, name, path, size, type }) => (
-        <div key={path} className='container'>
-            <img className='image-container' src={`${type.includes('application') ? 'https://upload.wikimedia.org/wikipedia/commons/0/0c/File_alt_font_awesome.svg': preview }`} alt='images prewiew' />
-            <h2 className='details'>
-                {`${name.substring(0, 12)}`} - {(size / 1024000).toFixed(2)}MB
-            </h2>
-        </div>
-    ))
-
+const FilePreview = ({ file }) => (
+    <div key={file?.path} className='container'>
+        <img
+            className='image-container'
+            src={`${file.type.includes('application') ? 'https://upload.wikimedia.org/wikipedia/commons/0/0c/File_alt_font_awesome.svg' : file?.preview}`}
+            alt={`${file.type} - ${file.preview}`}
+        />
+        <h2 className='details'>
+            {`${file?.name.substring(0, 12)}`} - {(file?.size / 1024000).toFixed(2)}MB
+        </h2>
+    </div>
 )
-export default FilesPreview
+
+export default FilePreview
