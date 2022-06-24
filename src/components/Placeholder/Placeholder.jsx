7fb9bc6d12f49a7117c5a1af6a3b4a7a796d4f47
Juan Omen <anvidmen@gmail.com>
2022-06-24 20:45:03 +0200

    added text of maximum files allowed

--- src/components/Placeholder/Placeholder.jsx
+++ src/components/Placeholder/Placeholder.jsx
@@ -5,6 +5,7 @@ const Placeholder = () => (
   <div className='placeholder-preview'>
     <MdCloudUpload className='icon' />
     <p>ARASTRA TUS ARCHIVOS AQUÍ</p>
+    <small>Sólo se permite un máximo de 9 archivos</small>
   </div>
 )
 
