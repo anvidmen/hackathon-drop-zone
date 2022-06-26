f64357ed3bec3ad284c99df314143a4507656ad2
Juan Omen <anvidmen@gmail.com>
2022-06-26 11:24:43 +0200

    implementing page routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import DropZone from "pages/Dropzone/Dropzone"
+import Register from "pages/Register/Register"
 
 const App = () => {
   return (
-    < >
-     <DropZone /> 
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' caseSensitive={false} element={<Register />} />
+        <Route path='/dropzone' caseSensitive={false} element={<DropZone />} />
+      </Routes>
+    </Router>
   )
 }
 
