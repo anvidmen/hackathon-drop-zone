48aef16352b4d409e42f9d9251cf7e8321de2a6f
Juan Omen <anvidmen@gmail.com>
2022-06-26 18:35:04 +0200

    routes added to main app and function for protected routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import retrieveUser from 'logic/retrieve-user'
 import DropZone from "pages/Dropzone/Dropzone"
 import Register from "pages/Register/Register"
+import Login from 'pages/Login/Login'
 
 const App = () => {
+  const user = retrieveUser()
+
+  const ProtectedRoute = ({ user, children }) => {
+    if (!user) {
+      return <Navigate to="/" replace />
+    }
+    return children;
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path='/' caseSensitive={false} element={<Register />} />
-        <Route path='/dropzone' caseSensitive={false} element={<DropZone />} />
+        <Route path='/' caseSensitive={false} element={<Login />} />
+        <Route path='/register' caseSensitive={false} element={<Register />} />
+        <Route path='/dropzone' caseSensitive={false} element={
+          <ProtectedRoute user={user}>
+            <DropZone />
+          </ProtectedRoute>
+        }
+        />
       </Routes>
     </Router>
   )
