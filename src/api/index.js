98e8a6a46b8377b56cebaa57f040de98e8814411
Juan Omen <anvidmen@gmail.com>
2022-06-26 18:22:12 +0200

    created the login endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,5 @@ import axios from 'axios'
 
 const baseURL = 'http://localhost:3004'
 
+export const login = data => axios.post(`${baseURL}/login`, data)
 export const register = data => axios.post(`${baseURL}/register`, data)
