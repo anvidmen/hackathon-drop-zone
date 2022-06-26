eb32784205b6ded6f59f3ebc82a6f27429a22da8
Juan Omen <anvidmen@gmail.com>
2022-06-26 23:41:25 +0200

    unused deleted files

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000'
+const baseURL = 'https://dropzone-server.herokuapp.com'
 
 export const login = data => axios.post(`${baseURL}/login`, data)
 export const register = data => axios.post(`${baseURL}/register`, data)
